8ea67699d1c97423d54724b2da5914ac16105e82
Anton Lazutin <laa.63@yandex.ru>
2023-10-19 17:14:12 +0300

    checking new ssh

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 const limiter = rateLimit({
   windowsMs: 15 * 60 * 1000,
