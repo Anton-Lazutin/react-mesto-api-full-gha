0d6ebe606cc6c78b1ef5f7de200240c07e21214c
Anton Lazutin <laa.63@yandex.ru>
2023-11-09 12:22:37 +0300

    fixed some bugs

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -6,11 +6,7 @@ const ForbiddenError = require('../errors/ForbiddenError');
 module.exports.addCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => {
-      Card.findById(card._id)
-        .populate('owner')
-        .then((data) => res.status(201).send(data));
-    })
+    .then((data) => res.status(201).send(data))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError(err.message));
@@ -22,7 +18,6 @@ module.exports.addCard = (req, res, next) => {
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .populate(['owner', 'likes'])
     .then((cards) => res.send(cards))
     .catch((err) => next(err));
 };
@@ -37,7 +32,7 @@ module.exports.deleteCards = (req, res, next) => {
         throw new ForbiddenError('Карточка другого пользователя');
       }
       Card.deleteOne(card)
-        .orFail(() => new Error('NotFound'))
+        .orFail(() => new NotFoundError())
         .then(() => {
           res.status(200).send({ message: 'Карточка удалена' });
         })
@@ -60,7 +55,6 @@ module.exports.deleteCards = (req, res, next) => {
 
 module.exports.likeCards = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .populate(['owner', 'likes'])
     .then((card) => {
       if (!card) {
         throw new NotFoundError('Карточка не найдена');
@@ -78,7 +72,6 @@ module.exports.likeCards = (req, res, next) => {
 
 module.exports.dislikeCards = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .populate(['owner', 'likes'])
     .then((card) => {
       if (!card) {
         throw new NotFoundError('Карточка не найдена');
